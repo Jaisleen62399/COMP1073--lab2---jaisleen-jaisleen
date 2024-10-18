e6eb26870605f24be1014653724df3ef38876ea1
Jaisleen62399 <156478290+Jaisleen62399@users.noreply.github.com>
2024-10-17 23:52:09 -0400

    Update page-color-changer.js
    const redInput, const greenInput, const blueInput

--- js/page-color-changer.js
+++ js/page-color-changer.js
@@ -1,2 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const redInput = document.getElementById('red');
+    const greenInput = document.getElementById('green');
+    const blueInput = document.getElementById('blue');
+    const colorValueDisplay = document.getElementById('color-value');
 
